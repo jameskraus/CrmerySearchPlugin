6341da3bff001a49c2db91596b15e906fd9de18e
James Kraus <jamespkraus@gmail.com>
2017-06-21 10:12:54 -0400

    Remove unneeded semicolons
    No one likes superfluous semicolons

--- make.sh
+++ make.sh
@@ -1,11 +1,11 @@
 (
     rm -rf build
     rm -rf temp
-    mkdir build;
-    mkdir temp;
-    cp -R src temp/crmery;
-    cd temp;
+    mkdir build
+    mkdir temp
+    cp -R src temp/crmery
+    cd temp
     zip -r ../build/PlgSearchCrmery.zip crmery/*
-    cd ..;
-    rm -rf temp;
+    cd ..
+    rm -rf temp
 )
